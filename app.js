53487c817905798dad059307802f52616226343f
JiamingXing <jx703@nyu.edu>
2017-10-25 15:46:45 -0700

    delete an item from our budget controller -- from our database

--- app.js
+++ app.js
@@ -69,6 +69,18 @@ var budgetController = (function() {
 			return newItem;
 		},
 
+		deleteItem : function(type, id) {
+			var ids, index;
+			ids = data.allItems[type].map(function(cur) {
+				return cur.id;
+			});
+			index = ids.indexOf(id);
+			//remove the exact index element in the array using splice
+			if (index !== -1) {
+				data.allItems[type].splice(index, 1);
+			}
+		},
+
 		calculateBudget : function() {
 			//calculate the sum of incomes and expenses
 			calculateTotal("exp");
@@ -127,7 +139,8 @@ var UIController = (function() {
 		budgetLabel : ".budget__value",
 		incomeLabel : ".budget__income--value",
 		expensesLabel : ".budget__expenses--value",
-		percentageLabel : ".budget__expenses--percentage"
+		percentageLabel : ".budget__expenses--percentage",
+		container : ".container"
 	}
 	return {
 		getInput : function() {
@@ -145,10 +158,10 @@ var UIController = (function() {
 			//1.create HTML string with place holder text
 			if (type === "inc") {
 				element = DOMStrings.incomeContainer;
-				html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+				html = '<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
 			} else if (type === "exp") {
 				element = DOMStrings.expenseContainer;
-				html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+				html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
 			}
 			//2. replace the place holder text with some actual data
 			newHtml = html.replace("%id%", obj.id);
@@ -207,7 +220,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 				ctrlAddItem();
 			}
 		});
-	}
+
+		document.querySelector(DOM.container).addEventListener("click", ctrlDeleteItem);
+	};
 
 	var updateBudget = function() {
 
@@ -217,7 +232,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 		var budget = budgetCtrl.getBudget();
 		//3. Display the budget
 		UICtrl.displayBudget(budget);
-	}
+	};
 
 	//event handler press 
 	var ctrlAddItem = function() {
@@ -241,7 +256,23 @@ var controller = (function(budgetCtrl, UICtrl) {
 		} else {
 			UICtrl.clearFields();
 		}
-	}
+	};
+
+	var ctrlDeleteItem = function(event) {
+		var itemID, splitID, type, ID;
+		itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+		if (itemID) {
+			splitID = itemID.split("-");
+			type = splitID[0];
+			ID = Number(splitID[1]);
+
+			//1. delete the item from the data structure
+			budgetCtrl.deleteItem(type, ID);
+			//2. delete the item from the UI
+
+			//3. update and show the new budget
+		}
+	};
 
 	//we need a public int function so return in the IIFE
 	return {
