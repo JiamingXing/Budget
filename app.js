5a2442a2992eada3ea9a45a4d0469dbf03cb20f0
JiamingXing <jx703@nyu.edu>
2017-10-25 17:15:03 -0700

    formatting our budget numbers

--- app.js
+++ app.js
@@ -169,6 +169,21 @@ var UIController = (function() {
 		container : ".container",
 		expensesPercLabel : ".item__percentage"
 	}
+
+	var formatNumber = function(num, type) {
+		var numSplit, int, dec, sign;
+		num = Math.abs(num);
+		num = num.toFixed(2);
+
+		numSplit = num.split(".");
+		int = numSplit[0];
+		if (int.length > 3) {
+			int = int.substr(0, int.length - 3) + "," + int.substr(int.length - 3, int.length);
+		}
+		dec = numSplit[1];
+		return (type === "exp" ? sign = "-" : sign = "+") + " " + int +  "." + dec;
+	};
+
 	return {
 		getInput : function() {
 			return {
@@ -193,7 +208,7 @@ var UIController = (function() {
 			//2. replace the place holder text with some actual data
 			newHtml = html.replace("%id%", obj.id);
 			newHtml = newHtml.replace("%description%", obj.description);
-			newHtml = newHtml.replace("%value%", obj.value);
+			newHtml = newHtml.replace("%value%", formatNumber(obj.value));
 			//3. Insert the HTML into the DOM
 			document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
 		},
@@ -216,9 +231,11 @@ var UIController = (function() {
 		},
 
 		displayBudget : function(obj) {
-			document.querySelector(DOMStrings.budgetLabel).textContent = obj.budget;
-			document.querySelector(DOMStrings.incomeLabel).textContent = obj.totalInc;
-			document.querySelector(DOMStrings.expensesLabel).textContent = obj.totalExp;
+			var tyep;
+			obj.budget > 0 ? type = "inc" : type = "exp";
+			document.querySelector(DOMStrings.budgetLabel).textContent = formatNumber(obj.budget, type);
+			document.querySelector(DOMStrings.incomeLabel).textContent = formatNumber(obj.totalInc, "inc");
+			document.querySelector(DOMStrings.expensesLabel).textContent = formatNumber(obj.totalExp, "exp");
 			if (obj.percentage > 0) {
 				document.querySelector(DOMStrings.percentageLabel).textContent = obj.percentage + "%";
 			} else {
